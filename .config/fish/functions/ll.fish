9dca7c8820a8462a9dfd3f3472be35d9af6af1e3
Adam Uhlir <uhlir.a@gmail.com>
2019-05-08 12:03:11 -0700

    Adding custom ll implementation that can open files

---
+++ .config/fish/functions/ll.fish
@@ -0,0 +1,18 @@
+function ll
+    if test (count $argv) -eq 0
+        ls -la
+        return
+    else if test (count $argv) -ne 1
+        echo "You must provide exactly one argument/path!"
+        return
+    end
+
+    set path $argv[1]
+
+    if test -d $path
+        ls -la $path
+    else
+        less $path
+    end
+end
+
