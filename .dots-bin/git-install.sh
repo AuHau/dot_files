46f7d2a5a314090ce5f598dea96bdae33bbd6f36
Adam Uhlíř <uhlir.a@gmail.com>
2019-06-02 16:03:32 -0700

    Fixing typos

--- .dots-bin/git-install.sh
+++ .dots-bin/git-install.sh
@@ -35,8 +35,8 @@ if [ $? = 0 ]; then
     echo "Backing up pre-existing dot files.";
     mkdir -p .dots-backup
     DOTS_FILES=`config checkout 2>&1 | egrep "\s+\." | awk {'print $1'}`
-    echo "$DOTS_FILES" | grep / |  xargs -I{} dirname {} | xargs-I{} mkdir -p .dots-backup/{}
-    echo "$DOTS_FILES" | xargs -I{} mv {} .dots-backup/{}
+    echo "$DOTS_FILES" | grep / |  xargs -I {} dirname {} | xargs -I {} mkdir -p .dots-backup/{}
+    echo "$DOTS_FILES" | xargs -I {} mv {} .dots-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
