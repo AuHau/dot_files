091e80e257ce76b1955ccb9fa8f572eaccac3c69
Adam Uhlíř <adam@uhlir.dev>
2024-08-08 11:35:01 +0200

    chore: ssh and done function

--- .config/fish/functions/done.fish
+++ .config/fish/functions/done.fish
@@ -1,4 +1,4 @@
 function done
-    osascript -e 'display notification "Your commands have finished!" with title "Done!"'
+    osascript -e 'display notification "Your commands have finished!" with title "Done!" sound name "Sonar"'
 end
 
